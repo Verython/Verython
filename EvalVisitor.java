0179c2709d5eea00542a1adaf8c5037c9453b288
maasencioh <maasencioh@gmail.com>
2016-05-25 01:58:39 -0500

    Block visitor

--- EvalVisitor.java
+++ EvalVisitor.java
@@ -6,11 +6,22 @@ public class EvalVisitor extends VerythonBaseVisitor<String> {
     Map<String, String> memory = new HashMap<String, String>();
 
     /*
-    if_stmt: IF test ':' suite ( ELIF test ':' suite )* ( ELSE ':' suite )?;
+    initial: NEWLINE* top EOF?;
     */
     @Override
     public String visitInitial(VerythonParser.InitialContext ctx) {
         System.out.println("`timescale 1ns/1ps");
+        return visitChildren(ctx);
+    }
+
+    /*
+    block: BLOCK ':' suite;
+    */
+    @Override
+    public String visitBlock(VerythonParser.BlockContext ctx) {
+        System.out.println("begin");
+        visit(ctx.suite());
+        System.out.println("end");
         return visitChildren(ctx);
     }
 }
