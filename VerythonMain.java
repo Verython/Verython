64a42c5ea77df8a9d58e0cbc3687064eec5685aa
maasencioh <maasencioh@gmail.com>
2016-05-24 20:12:13 -0500

    Main file an a simple test

---
+++ VerythonMain.java
@@ -0,0 +1,18 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class VerythonMain {
+    public static void main( String[] args) throws Exception
+    {
+        try {
+            VerythonLexer lexer = new VerythonLexer(new ANTLRFileStream(args[0]));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            VerythonParser parser = new VerythonParser(tokens);
+            ParseTree tree = parser.initial();
+            System.out.println(tree.toStringTree(parser));
+        }
+        catch (Exception e) {
+            System.err.println("Error (Verython): "+e);
+        }
+    }
+}
